715b8f80cb4afa5431fcdb1f538506f0e95833b2
GabyFCE <duartegabriel2017@gmail.com>
2023-04-01 22:56:34 -0300

    Add project files.

---
+++ Ejercicios.Ejercicio3/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Ingrese lo que quiera, presione X para salir");
+while (true) 
+    {
+
+    if (Console.ReadKey().KeyChar == 'X') 
+         {
+         break;
+         }
+    }
